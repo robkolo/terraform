aa6a4eb891d571cd34cbb34279a25b49adac419d
Robert Kolodziejczyk <95703053+robkolo@users.noreply.github.com>
2022-09-13 08:29:30 -0500

    Add files via upload

---
+++ basic-aws-setup-v1-marshall/variables.tf
@@ -0,0 +1,17 @@
+
+variable "aws_access_key" {
+  type = string
+}
+
+variable "aws_secret_key" {
+  type = string
+}
+
+variable "access_ip" {
+  type = string
+}
+
+variable "aws_region" {
+  type    = string
+  default = "eu-central-1"
+}
